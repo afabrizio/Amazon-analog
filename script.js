f1e9977d457ab7ebad2536c998cb9716fe4425c8
afabrizio <andrew.m.fabrizio@gmail.com>
2016-08-31 16:05:41 -0700

    Added a functioning Add-To-Cart feature. #2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var theSearchButton = document.getElementById('search');
 var theData = JSON.parse(data); //parses JSON string data to javascript objects.
 var searchMatches = []; // an array that contains 'theData' object indicies of matches identified by search().
 
-//Adds all the theData objects on the DOM:
+//Initially, upon page load, adds all the theData objects on the DOM:
 for (var i=0; i<theData.length; i++) {
   addDataElement(i);
 }
@@ -19,10 +19,10 @@ function addDataElement(index) {
   var elementDetailsDiv = document.createElement('div');
   elementDetailsDiv.id = 'object-details';
   dataElement.appendChild(elementDetailsDiv);
-  var cartDiv = document.createElement('button');
-  cartDiv.id = 'add-to-cart';
-  cartDiv.className = 'fa fa-cart-plus fa-2x added';
-  dataElement.appendChild(cartDiv);
+  var cartButton = document.createElement('button');
+  cartButton.id = 'add-to-cart';
+  cartButton.className = 'fa fa-cart-plus fa-2x added ' + 'item-identifier-' + index.toString(); //stores item identifier as a class in the element, to be referenced for the addToCart feature.
+  dataElement.appendChild(cartButton);
 
   var objectImage = document.createElement('img');
   objectImage.src = theData[index].image;
@@ -53,7 +53,7 @@ function addDataElement(index) {
   dataElements.appendChild(dataElement);
 }
 
-//Search Engine
+/* ------Search feature: ------ */
 theSearchButton.addEventListener('click', search);
 function search() {
   clearResults(); //first clears the current products from the DOM
@@ -98,4 +98,59 @@ function clearResults() {
     dataElements.removeChild(dataElements.firstChild);
   }
   searchMatches.length = 0; //Clear the SearchMatches array
+}
+
+/* ------Add to Cart feature: ------ */
+var cart = [];//creates the cart, which is an array of objects, each with two properties, item & item_qty.
+
+//Listens for an addToCart click, shows object that fired the event.
+document.getElementById('data-elements').addEventListener('click', function() {
+  if (event.target.type === 'submit') {//this conditional checks if the event.target is an addToCart button:
+    var classNames = event.target.className;
+    var classNames = classNames.split(' ');
+    for (var i=0; i<classNames.length; i++) { //loops through all the classes...
+      if (classNames[i].indexOf('item-identifier') !== -1) { //checks for an item-identifier class
+        var itemIdentifier = classNames[i];
+        var dataObjectIndex = itemIdentifier.split('-');
+        var dataObjectIndex = dataObjectIndex[2]; //saves the object index of the item that was added to the cart.
+      }
+    }
+    //This code creates/modifies the cart[] objects as appropriate:
+    for (var i=0; i<cart.length; i++) { //loops through cart to check if item already exists in cart:
+      if(cart[i].itemId === dataObjectIndex) { //if item already is in the cart...
+        cart[i].qty += 1; //...increment the qty.
+        CartButtonToGreen(dataObjectIndex); //changes color of addToCart button to green.
+        updateCartIcon(getQty()); //updates the cart icon with the number of items in currently in the cart.
+        return; //exit the function.
+      }
+    }
+    var newCartItem = {}; //otherwise makes a new cart object for the item...
+    newCartItem.itemId = dataObjectIndex; //...gives it new properties...
+    newCartItem.qty = 1;
+    cart.push(newCartItem); //...and appends the object to the end of the cart array.
+    CartButtonToGreen(dataObjectIndex); //changes color of addToCart button to green.
+    updateCartIcon(getQty()); //updates the cart icon with the number of items in currently in the cart.
+  }
+  //changes addToCart button to green when user adds something to his/her cart.
+  function CartButtonToGreen(itemId) {
+    target = event.target;
+    if (target.classList.contains('item-identifier-' + itemId)){
+      target.className += ' item-added';
+    }
+  }
+});
+
+//gets the quantity of items in the cart.
+function getQty() {
+  var cartQty = 0;
+  for (i=0; i<cart.length; i++) {
+    cartQty = cartQty += parseInt(cart[i].qty);
+  }
+  return cartQty;
+}
+
+//updates the qty in the cart icon on the navbar;
+function updateCartIcon(qty) {
+  cartQty = document.getElementById('view-cart');
+  cartQty.textContent = ' ' + qty;
 }
